b84e69810110ecc23d43b65d2b25559f0a694db3
fokaefs <fokaefs@ualberta.ca>
2012-01-12 20:00:35 -0700

    Added TextView

--- AndroidSandbox/src/ca/ualberta/cs/serl/AndroidSandboxActivity.java
+++ AndroidSandbox/src/ca/ualberta/cs/serl/AndroidSandboxActivity.java
@@ -2,12 +2,15 @@ package ca.ualberta.cs.serl;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AndroidSandboxActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello Android");
         setContentView(R.layout.main);
     }
 }
